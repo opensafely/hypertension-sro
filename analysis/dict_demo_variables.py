9837b65071d126871a2ec8d3bb588a9df6bd1ebe
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-02 14:08:20 +0000

    add placeholder dictionaties for common variables

---
+++ analysis/dict_demo_variables.py
@@ -0,0 +1,19 @@
+# Define common demographic variables needed across indicators here
+# See https://docs.opensafely.org/study-def-tricks/#sharing-common-study-definition-variables
+
+demographic_variables = dict(
+
+  # Age
+  # TODO
+  # Sex
+  # TODO
+  # Index of Multiple Deprivation (IMD)
+  # TODO
+  # Region
+  # TODO
+  # Ethnicity
+  # TODO
+  # Practice
+  # TODO
+
+)
---
+++ analysis/dict_hyp_variables.py
@@ -0,0 +1,39 @@
+# Define common variables needed across indicators here
+# See https://docs.opensafely.org/study-def-tricks/#sharing-common-study-definition-variables
+
+hyp_variables = dict(
+
+  # Rules for indicators HYP003 and HYP007
+  # The only differences between the indicators are:
+  # - Age (Denominator rule 1)
+  #   - HYP003: >79
+  #   - HYP007: <80
+  # - Values of last blood pressure (Denominator rules 2, 7)
+  #   - HYP003: SYS<= 140, DIA <= 90
+  #   - HYP007: SYS <= 150, DIA <= 90
+
+  # Denominator
+  # Rule 1
+  # TODO
+  # Rule 2
+  # TODO
+  # Rule 3
+  # TODO
+  # Rule 4
+  # TODO
+  # Rule 5
+  # TODO
+  # Rule 6
+  # TODO
+  # Rule 7
+  # TODO
+  # Rule 8
+  # TODO
+  # Rule 9
+  # TODO
+
+  # Numerator
+  # Rule 1
+  # TODO
+
+)
