2ae3a077c53e0b4388dc55818ba90ab6ddd9cea4
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-03 17:28:46 +0000

    add basic structure for study definition for hyp register

--- analysis/study_definition_hyp001.py
+++ analysis/study_definition_hyp001.py
@@ -1,20 +1,30 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+
+from config import start_date, end_date
 from dict_hyp_variables import hyp_variables
 from dict_demo_variables import demographic_variables
 
 study = StudyDefinition(
+    index_date=start_date,
+
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": start_date, "latest": end_date},
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+
+    population=patients.satisfying(
+        """
+        # Define general population parameters
+        (sex = 'F' OR sex = 'M')
+
+        """,
     ),
 
     # Include hypertension variables
-    # ** hyp_variables
+    ** hyp_variables,
 
     # Include demographic variables
-    # ** demographic_variables
+    ** demographic_variables
+
 )
