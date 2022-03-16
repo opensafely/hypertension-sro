20f8f864ed01a04ca437e46e50cbec7a7eac662c
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-16 14:38:23 +0000

    load codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,3 +1,64 @@
 from cohortextractor import codelist_from_csv
 
-# TODO READ CODELISTS HERE
+bp_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-bp_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+bpdec_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-bpdec_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+htmax_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-htmax_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+hyp_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-hyp_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+hypinvite_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-hypinvite_cod.csv",
+    system="snomed",
+    column="code",
+)
+hyppcadec_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-hyppcadec_cod.csv",
+    system="snomed",
+    column="code",
+)
+hyppcapu_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-hyppcapu_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+hypres_codes = codelist_from_csv(
+    "codelists/nhsd-primary-care-domain-refsets-hypres_cod.csv",
+    system="snomed",
+    column="code",
+)
+
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="snomed",
+    column="code",
+)
+
+ld_codes = codelist_from_csv(
+    "codelists/opensafely-learning-disabilities.csv",
+    system="snomed",
+    column="code",
+)
+carehome_codes = codelist_from_csv(
+    "codelists/opensafely-nhs-england-care-homes-residential-status.csv",
+    system="snomed",
+    column="code",
+)
