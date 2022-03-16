2d4e8ccc4e0b2c193f33f303a3a2246d4fe1090a
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-16 15:29:55 +0000

    add more descriptions to codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,47 +1,76 @@
 from cohortextractor import codelist_from_csv
 
+# Cluster name: BP_COD
+# Description: Blood pressure (BP) recording codes
+# SNOMED CT: ^999012731000230108
 bp_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-bp_cod.csv",
     system="snomed",
     column="code",
     )
 
+# Cluster name: BPDEC_COD
+# Description: Codes indicating the patient has chosen not to have blood pressure procedure
+# SNOMED CT: ^999012611000230106
 bpdec_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-bpdec_cod.csv",
     system="snomed",
     column="code",
     )
 
+
+# Cluster name: HTMAX_COD
+# Description: Codes for maximal blood pressure (BP) therapy
+# SNOMED CT: ^999006651000230109
 htmax_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-htmax_cod.csv",
     system="snomed",
     column="code",
     )
 
+
+# Cluster name: HYP_COD
+# Description: Hypertension diagnosis codes
+# SNOMED CT: ^999006611000230105
 hyp_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-hyp_cod.csv",
     system="snomed",
     column="code",
     )
 
+
+# Cluster name: HYPINVITE_COD
+# Description: Invite for hypertension care review codes
+# SNOMED CT: ^999012971000230108
 hypinvite_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-hypinvite_cod.csv",
     system="snomed",
     column="code",
     )
 
+
+# Cluster name: HYPPCADEC_COD
+# Description: Codes indicating the patient has chosen not to receive hypertension quality indicator care
+# SNOMED CT: ^999013091000230102
 hyppcadec_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-hyppcadec_cod.csv",
     system="snomed",
     column="code",
     )
 
+
+# Cluster name: HYPPCAPU_COD
+# Description: Codes for hypertension quality indicator care unsuitable for patient
+# SNOMED CT: ^999013211000230104
 hyppcapu_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-hyppcapu_cod.csv",
     system="snomed",
     column="code",
     )
 
+# Cluster name: HYPRES_COD
+# Description: Hypertension resolved codes
+# SNOMED CT: ^999006531000230101
 hypres_codes = codelist_from_csv(
     "codelists/nhsd-primary-care-domain-refsets-hypres_cod.csv",
     system="snomed",
