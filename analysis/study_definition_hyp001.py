8e221da4fa6ca862adf451217b39bd958815ca5f
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-17 10:31:09 +0000

    variables from study def moved to dictionaries

--- analysis/study_definition_hyp001.py
+++ analysis/study_definition_hyp001.py
@@ -1,6 +1,11 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv, Measure  # NOQA
+
+import json
+import pandas as pd
 
 from config import start_date, end_date
+from codelists import hyp_codes, hypres_codes
+
 from dict_hyp_reg_variables import hyp_reg_variables
 from dict_demo_variables import demographic_variables
 
@@ -19,23 +24,12 @@ study = StudyDefinition(
         # Define general population parameters
         registered AND
         (NOT died) AND
-        (sex = 'F' OR sex = 'M')
+        (sex = 'F' OR sex = 'M') AND
 
         (hyp = 1) AND
         (hyp_res = 0)
         """,
     ),
-
-    registered=patients.registered_as_of(
-        "index_date",
-        return_expectations={"incidence": 0.9},
-        ),
-
-    died=patients.died_from_any_cause(
-        on_or_before="index_date",
-        returning="binary_flag",
-        return_expectations={"incidence": 0.1}
-        ),
 
     # Include hypertension variables
     ** hyp_reg_variables,
