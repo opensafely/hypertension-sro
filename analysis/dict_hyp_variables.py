9d8f8b3fd4fa69f6e5d81a05899f7269dafd8001
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-18 10:35:04 +0000

    move all hyp dict vars into one file

--- analysis/dict_hyp_variables.py
+++ analysis/dict_hyp_variables.py
@@ -11,7 +11,6 @@ hyp_codes_unique = hyp_codes_df['code'].unique()
 
 # Hypertension register:
 # Patients with an unresolved diagnosis of hypertension
-
 hyp_reg_variables = dict(
 
   registered=patients.registered_as_of(
@@ -41,4 +40,49 @@ hyp_reg_variables = dict(
     returning="binary_flag",
     ),
 
+  hyp001_indicator=patients.satisfying(
+    """
+    hypertension AND
+    (NOT hypertension_resolved)
+    """
+  )
+
     )
+
+hyp_ind_variables = dict(
+
+  # Rules for indicators HYP003 and HYP007
+  # The only differences between the indicators are:
+  # - Age (Denominator rule 1)
+  #   - HYP003: >79
+  #   - HYP007: <80
+  # - Values of last blood pressure (Denominator rules 2, 7)
+  #   - HYP003: SYS <= 140, DIA <= 90
+  #   - HYP007: SYS <= 150, DIA <= 90
+
+  # Denominator
+  # Rule 1
+  # Age is defined in demographic_variables
+
+  # Rule 2
+  # TODO
+  # Rule 3
+  # TODO
+  # Rule 4
+  # TODO
+  # Rule 5
+  # TODO
+  # Rule 6
+  # TODO
+  # Rule 7
+  # TODO
+  # Rule 8
+  # TODO
+  # Rule 9
+  # TODO
+
+  # Numerator
+  # Rule 1
+  # TODO
+)
+
