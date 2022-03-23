8f86db9ba600c0a71d4bc06b9db30aa7d7468afe
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-23 10:31:19 +0000

    add join measures action

---
+++ analysis/join_measures.R
@@ -0,0 +1,45 @@
+# Load packages ----
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(here)
+library(readr)
+library(fs)
+library(purrr)
+library(stringr)
+
+# Get file names and path ----
+dir_hyp_001_measures <- fs::dir_ls(path = "output/",
+                                   glob = "*hyp001*.csv$")
+
+# Split dir paths because file structure differes
+## Grouped measures
+dir_hyp_001_measures_groups <- dir_hyp_001_measures[!stringr::str_detect(dir_hyp_001_measures, "population")]
+## Population measure 
+dir_hyp_001_measures_pop <- dir_hyp_001_measures[stringr::str_detect(dir_hyp_001_measures, "population")]
+
+# Load files ----
+## Load grouped measures
+## Pivot longer so variable names are identical across measure files
+df_hyp_001_measures_groups <- dir_hyp_001_measures_groups %>%
+  purrr::map(readr::read_csv) %>%
+  purrr::map_dfr(tidyr::pivot_longer,
+          cols = 1,
+          names_to = "group",
+          values_to = "category",
+          values_transform = list(category = as.character))
+
+# Load population measure ---
+# Add variables that are missing compared to grouped measures
+df_hyp_001_measures_pop <- readr::read_csv(dir_hyp_001_measures_pop) %>%
+  dplyr::mutate(group = "pupulation",
+         category = "population")
+
+# Join all measures into one object ---
+df_hyp_001_measures <- df_hyp_001_measures_groups %>%
+  dplyr::bind_rows(df_hyp_001_measures_pop)
+
+# Write csv
+readr::write_csv(df_hyp_001_measures,
+                 here::here("output/measures/measures_hyp001.csv"))
