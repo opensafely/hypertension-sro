2392efeaceb96f279534e34118ebed1acf384585
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-03-23 12:02:55 +0000

    fix join population

--- analysis/join_measures.R
+++ analysis/join_measures.R
@@ -25,16 +25,16 @@ dir_hyp_001_measures_pop <- dir_hyp_001_measures[stringr::str_detect(dir_hyp_001
 df_hyp_001_measures_groups <- dir_hyp_001_measures_groups %>%
   purrr::map(readr::read_csv) %>%
   purrr::map_dfr(tidyr::pivot_longer,
-          cols = 1,
-          names_to = "group",
-          values_to = "category",
-          values_transform = list(category = as.character))
+                 cols = 1,
+                 names_to = "group",
+                 values_to = "category",
+                 values_transform = list(category = as.character))
 
 # Load population measure ---
 # Add variables that are missing compared to grouped measures
 df_hyp_001_measures_pop <- readr::read_csv(dir_hyp_001_measures_pop) %>%
-  dplyr::mutate(group = "pupulation",
-         category = "population")
+  dplyr::mutate(group = "population",
+                category = "population")
 
 # Join all measures into one object ---
 df_hyp_001_measures <- df_hyp_001_measures_groups %>%
