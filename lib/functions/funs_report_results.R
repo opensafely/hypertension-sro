e1af1f3d86785d7cc4af626a3fc45eb4a6559c00
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-26 15:57:29 +0300

    add documentation of fun report

--- lib/functions/funs_report_results.R
+++ lib/functions/funs_report_results.R
@@ -1,11 +1,26 @@
-
-report_measures <- function(df, value, date, group, category, convert_percent = TRUE) {
+#' Report count and percentage of measures
+#'
+#' @param df Data frame. Requires the variables named "date", "group", "category" used for filtering
+#' @param var_value Name of variable with count or percent values
+#' @param date String, specifying value for filtering date
+#' @param group String, specifying value for filtering date
+#' @param category String, specifying value for filtering date
+#' @param convert_percent Logical, whether to convert percent (0.1) to 10.00%
+#'
+#' @return
+#' @export
+#'
+#' @examples
+report_measures <- function(df, 
+                            var_value, 
+                            filter_date, filter_group, filter_category, 
+                            convert_percent = TRUE) {
   
   report_value <- df %>% 
-    filter(date == {{ date }}) %>% 
-    filter(group == {{ group }}) %>% 
-    filter(category == {{ category }}) %>% 
-    pull({{ value }})
+    filter(date == filter_date) %>% 
+    filter(group == filter_group) %>% 
+    filter(category == filter_category) %>% 
+    pull({{ var_value }})
   
   if (convert_percent) {
     
