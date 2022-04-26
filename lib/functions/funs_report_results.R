b89f26aff89647caa52075e3e53e1e92ea614d03
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-04-26 14:31:02 +0300

    add report measures function

---
+++ lib/functions/funs_report_results.R
@@ -0,0 +1,22 @@
+
+report_measures <- function(df, value, date, group, category, convert_percent = TRUE) {
+  
+  report_value <- df_measures_hyp001 %>% 
+    filter(date == {{ date }}) %>% 
+    filter(group == {{ group }}) %>% 
+    filter(category == {{ category }}) %>% 
+    pull({{ value }})
+  
+  if (convert_percent) {
+    
+    if (report_value <= 1) {
+      report_value <-  scales::percent(report_value, accuracy = 0.01)
+    } else {
+        warning(paste0("value is greater than 1 (", report_value, ") and not converted to %."), 
+                call. = FALSE)
+      }
+    }
+  
+  return(report_value)
+  
+  }
