522d92caf31b63cb3a7e42c9fd7ec491b6e88b61
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-05-17 17:19:15 +0100

    fix check data script

--- analysis/check_bp_recording_values_dates.R
+++ analysis/check_bp_recording_values_dates.R
@@ -11,7 +11,9 @@ library(readr)
 df_hyp003_2019_01_01 <- read_feather(here("output/indicators/joined/input_hyp003_2019-01-01.feather"))
 
 df_hyp003_2019_01_01 <- df_hyp003_2019_01_01 %>%
-  select(patient_id, bp_sys_val_12m_date_measured,
+  select(patient_id,
+         hyp003_numerator,
+         bp_sys_val_12m_date_measured,
          bp_dia_val_12m_date_measured,
          bp_sys_val_12m,
          bp_dia_val_12m)
@@ -20,10 +22,9 @@ tab_hyp003_2019_01_01 <- df_hyp003_2019_01_01 %>%
   filter(hyp003_numerator == TRUE) %>%
   mutate(sys_recording = !is.na(bp_sys_val_12m_date_measured),
          dia_recording = !is.na(bp_dia_val_12m_date_measured)) %>%
-  select(patient_id, sys_recording, dia_recording) %>%
-  pivot_longer(cols = c(sys_recording, dia_recording)) %>%
-  group_by(name) %>%
-  count(available = value == TRUE)
+  select(sys_recording, dia_recording) %>%
+  group_by(sys_recording, dia_recording) %>%
+  count()
 
 fs::dir_create(here("output", "indicators", "joined", "data_check"))
 write_csv(tab_hyp003_2019_01_01, here("output", "indicators", "joined", "data_check", "tab_hyp003_2019_01_01.csv"))
