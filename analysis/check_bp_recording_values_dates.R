13d02d13d1f488d55b221f623f78b09fbce1a8f1
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-05-18 17:15:03 +0100

    feat: improve check data documentation and plot

--- analysis/check_bp_recording_values_dates.R
+++ analysis/check_bp_recording_values_dates.R
@@ -1,4 +1,4 @@
-
+# Load pacakges
 library(arrow)
 library(here)
 library(dplyr)
@@ -8,8 +8,12 @@ library(ggplot2)
 library(magrittr)
 library(readr)
 
+# Import data 
+## Indicator hyp003
+## Date: 2019-01-01
 df_hyp003_2019_01_01 <- read_feather(here("output/indicators/joined/input_hyp003_2019-01-01.feather"))
 
+# Select variables
 df_hyp003_2019_01_01 <- df_hyp003_2019_01_01 %>%
   select(patient_id,
          hyp003_numerator,
@@ -18,21 +22,43 @@ df_hyp003_2019_01_01 <- df_hyp003_2019_01_01 %>%
          bp_sys_val_12m,
          bp_dia_val_12m)
 
+# Count available bp recordings for sys and dia readings in numerator
 tab_hyp003_2019_01_01 <- df_hyp003_2019_01_01 %>%
   filter(hyp003_numerator == TRUE) %>%
   mutate(sys_recording = !is.na(bp_sys_val_12m_date_measured),
          dia_recording = !is.na(bp_dia_val_12m_date_measured)) %>%
   select(sys_recording, dia_recording) %>%
   group_by(sys_recording, dia_recording) %>%
-  count()
+  count() %>%
+  ungroup() %>%
+  mutate(pct = n / sum(n))
 
+# Create dir (if it doesnt exist)
 fs::dir_create(here("output", "indicators", "joined", "data_check"))
+
+# Write csv with counts
 write_csv(tab_hyp003_2019_01_01, here("output", "indicators", "joined", "data_check", "tab_hyp003_2019_01_01.csv"))
 
-bp_date_diff_2019_01_01 <- df_hyp003_2019_01_01 %>%
-  mutate(bp_date_diff = as.duration(bp_sys_val_12m_date_measured - bp_dia_val_12m_date_measured)) %>%
-  ggplot(aes(bp_date_diff)) +
-  geom_histogram() +
-  scale_x_time()
+# Calculate absolute diff (in days) between systolic and diastolic bp readings
+df_bp_date_diff_2019_01_01 <- df_hyp003_2019_01_01 %>%
+  mutate(bp_date_diff = abs(as.duration(bp_sys_val_12m_date_measured - bp_dia_val_12m_date_measured) / ddays()))
 
-ggsave(here("output", "indicators", "joined", "data_check", "bp_date_diff_2019_01_01.png"), plot = bp_date_diff_2019_01_01)
+tab_bp_date_diff_2019_01_01 <- df_bp_date_diff_2019_01_01 %>%
+  mutate(diff_weeks = case_when(bp_date_diff <= 7 ~ "1 week",
+                                bp_date_diff <= 30 ~ "1 month",
+                                bp_date_diff > 30 ~ "more than 1 month")) %>%
+  count(diff_weeks) %>%
+  mutate(pct = n / sum(n))
+
+# Write csv with counts and pct
+write_csv(tab_bp_date_diff_2019_01_01, here("output", "indicators", "joined", "data_check", "tab_bp_date_diff_2019_01_01.csv"))
+
+
+plot_bp_date_diff_2019_01_01 <- df_bp_date_diff_2019_01_01 %>%
+  ggplot(aes(bp_date_diff)) +
+  labs(x = "Absolute difference in days between systolic and diastolic blood pressure readings") +
+  geom_histogram()
+
+# Save plot
+ggsave(here("output", "indicators", "joined", "data_check", "bp_date_diff_2019_01_01.png"),
+       plot = plot_bp_date_diff_2019_01_01)
