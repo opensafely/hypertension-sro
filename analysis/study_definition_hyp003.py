7bc3e300ec744cb19f62555129877b9d8cfdc3ed
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-05-27 16:47:19 +0100

    fix: match select / reject of ind hyp003 and hyp007

--- analysis/study_definition_hyp003.py
+++ analysis/study_definition_hyp003.py
@@ -49,28 +49,28 @@ study = StudyDefinition(
         # Actions in business rules: True: Select; False: Reject
         hyp003_denominator_r1 AND
 
-            # Actions in business rules: True: Select; False: Next
-            (hyp003_denominator_r2 OR
+        # Actions in business rules: True: Select; False: Next
+        (hyp003_denominator_r2 OR
 
             (
                 (
-                # Actions in business rules: True: Reject; False: Next
-                hyp003_denominator_r3 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp003_denominator_r3 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp003_denominator_r4 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp003_denominator_r4 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp003_denominator_r5 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp003_denominator_r5 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp003_denominator_r6 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp003_denominator_r6 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp003_denominator_r7 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp003_denominator_r7 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp003_denominator_r8
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp003_denominator_r8
                 ) AND
 
                 # Actions in business rules: True: Reject; False: Select
--- analysis/study_definition_hyp007.py
+++ analysis/study_definition_hyp007.py
@@ -49,27 +49,29 @@ study = StudyDefinition(
         # Actions in business rules: True: Select; False: Reject
         hyp007_denominator_r1 AND
 
-            # Actions in business rules: True: Select; False: Next
-            (hyp007_denominator_r2 OR
+        # Actions in business rules: True: Select; False: Next
+        (hyp007_denominator_r2 OR
 
             (
-                # Actions in business rules: True: Reject; False: Next
-                hyp007_denominator_r3 OR
+                (
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp007_denominator_r3 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp007_denominator_r4 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp007_denominator_r4 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp007_denominator_r5 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp007_denominator_r5 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp007_denominator_r6 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp007_denominator_r6 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                # hyp007_denominator_r7 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp007_denominator_r7 OR
 
-                # Actions in business rules: True: Reject; False: Next
-                hyp007_denominator_r8 OR
+                    # Actions in business rules: True: Reject; False: Next
+                    hyp007_denominator_r8
+                ) AND
 
                 # Actions in business rules: True: Reject; False: Select
                 # NOTE: This rule is coded reversely so that:
