312fb755ad87c39009ea81e55231129ebf8c04a5
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-06-11 23:56:54 +0100

    extend extraction to entire study period from fy 19/20 to 21/22

--- analysis/config.py
+++ analysis/config.py
@@ -1,12 +1,12 @@
 # Study start date
 # Note: This should match the start dates in project.yaml
 # In QOF also: Payment Period Start Date (PPSD)
-start_date = "2020-03-01"
+start_date = "2019-03-01"
 
 # Study end date
 # Note: This should match the end dates in project.yaml
 # In QOF also: Payment Period End Date (PPED)
-end_date = "2020-05-01"
+end_date = "2022-04-01"
 
 # Demographic variables by which measures are broken down
 demographic_breakdowns = [
