e550c41320bcaf32e0f4acd7dfadb35cd851fe74
milanwiedemann <milan.wiedemann@gmail.com>
2022-09-15 16:18:28 +0100

    move report resutls funs into r script

--- lib/functions/funs_report_results.R
+++ lib/functions/funs_report_results.R
@@ -11,27 +11,81 @@
 #' @export
 #'
 #' @examples
-report_measures <- function(df, 
-                            var_value, 
-                            filter_date, filter_group, filter_category, 
+report_measures <- function(df,
+                            var_value,
+                            filter_date, filter_group, filter_category,
                             convert_percent = TRUE) {
-  
-  report_value <- df %>% 
-    filter(date == filter_date) %>% 
-    filter(group == filter_group) %>% 
-    filter(category == filter_category) %>% 
+  report_value <- df %>%
+    filter(date == filter_date) %>%
+    filter(group == filter_group) %>%
+    filter(category == filter_category) %>%
     pull({{ var_value }})
-  
+
   if (convert_percent) {
-    
     if (report_value <= 1) {
-      report_value <-  scales::percent(report_value, accuracy = 0.01)
+      report_value <- scales::percent(report_value, accuracy = 0.01)
     } else {
-        warning(paste0("value is greater than 1 (", report_value, ") and not converted to %."), 
-                call. = FALSE)
-      }
+      warning(paste0("value is greater than 1 (", report_value, ") and not converted to %."),
+        call. = FALSE
+      )
     }
-  
-  return(report_value)
-  
   }
+
+  return(report_value)
+}
+
+summary_report_min_max <- function(df, filter_group, filter_variable = c("pct", "numerator", "denominator", "population", "decile")) {
+  df_min <- df |>
+    filter(group == {{ filter_group }}) |>
+    filter(variable == {{ filter_variable }}) |>
+    group_by(indicator, variable) |>
+    slice_min(value, n = 1)
+
+  df_max <- df |>
+    filter(group == {{ filter_group }}) |>
+    filter(variable == {{ filter_variable }}) |>
+    group_by(indicator, variable) |>
+    slice_max(value, n = 1)
+
+  df_diff <- bind_rows(df_min, df_max) |>
+    arrange(indicator, date, variable) |>
+    group_by(indicator, variable) |>
+    mutate(diff = value - lag(value))
+
+  df_diff
+}
+
+
+report_national_median <- function(df, filter_date = NULL) {
+  df <- df |>
+    filter(variable == "decile") |>
+    filter(category == "50") |>
+    pivot_wider(names_from = indicator, values_from = value)
+
+  if (is.null(filter_date)) {
+    df
+  } else {
+    df |>
+      filter(date == filter_date)
+  }
+}
+
+report_group_pct <- function(df, filter_group, filter_category = NULL, filter_date = NULL) {
+  df <- df |>
+    filter(variable == "pct") |>
+    filter(group == filter_group) |>
+    pivot_wider(names_from = indicator, values_from = value)
+
+
+  if (!is.null(filter_category)) {
+    df <- df |>
+      filter(category == filter_category)
+  }
+
+  if (is.null(filter_date)) {
+    df
+  } else {
+    df |>
+      filter(date == filter_date)
+  }
+}
