d03d832e4211b5bb0f79d3ee43d572f4978fe33c
Milan Wiedemann <milan.wiedemann@gmail.com>
2023-04-06 17:33:15 +0100

    Extend study date range
    This includes 4 compete NHS financial years.

--- analysis/config.py
+++ analysis/config.py
@@ -1,12 +1,12 @@
 # Study start date
 # Note: This should match the start dates in project.yaml
 # In QOF also: Payment Period Start Date (PPSD)
-start_date = "2019-03-01"
+start_date = "2019-04-01"
 
 # Study end date
 # Note: This should match the end dates in project.yaml
 # In QOF also: Payment Period End Date (PPED)
-end_date = "2022-10-01"
+end_date = "2023-03-31"
 
 # Demographic variables by which measures are broken down
 demographic_breakdowns = [
